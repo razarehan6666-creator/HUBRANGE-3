6bc8129b1dbb3cad36b4ab6fad89258cc9ff2142
razarehan6666-creator <razarehan6666@gmail.com>
2025-10-25 02:03:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def month_data(month_name):
         return jsonify({"error": "No data found for this month"})
 
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", debug=True)
+
 
